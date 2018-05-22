d05c5b47ed0976342632412b9e2efb33f09d6161
waka93 <geniuswrt@gmail.com>
2018-05-22 00:24:56 -0500

    Add files via upload

---
+++ QiongYou/config.py
@@ -0,0 +1,8 @@
+BASE_URL = 'http://bbs.qyer.com/forum-53-{}.html'
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36'
+}
+
+POOL_REFRESHING_TIME = 40
+SLEEP_CYCLE = 60
+MAX_COUNT = 10
